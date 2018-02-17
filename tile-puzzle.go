59dcc86a24f4b64af1a7aef96d23379a96f0748d
Roy Knäbel <Roy.Knaebel@gmx.de>
2018-02-17 12:32:00 +0100

    print board

--- tile-puzzle.go
+++ tile-puzzle.go
@@ -1,17 +1,36 @@
 package main
 
-board = []int16
+import (
+	"fmt"
+)
 
 func main() {
-	board = 
+	var board [9]int = [9]int{1, 2, 3, 4, 5, 6, 7, 8, 0}
+	initBoard(&board)
 }
 
-func initBoard(board *[]int16) []int16 {
+func initBoard(board *[9]int) {
+	for i, v := range board {
+		fmt.Printf("%d %d\n", i, v)
+	}
+}
+
+func printBoard(board *[9]int) {
 
 }
 
-func printBoard(board *[]int16) {
+func left(board *[9]int) {
 
 }
 
+func right(board *[9]int) {
 
+}
+
+func up(board *[9]int) {
+
+}
+
+func down(board *[9]int) {
+
+}
