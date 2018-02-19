333188003d990c11fa5e6654e971ba28e5c38d45
Roy Knäbel <roy.knaebel@gmx.de>
2018-02-19 15:55:06 +0100

    add test for contains function

--- tile-puzzle_test.go
+++ tile-puzzle_test.go
@@ -2,6 +2,20 @@ package main
 
 import "testing"
 
+func TestContains(t *testing.T) {
+	board := [9]int{1, 2, 3, 4, 5, 6, 7, 8, 0}
+	for i := range board {
+		if !contains(&board, i) {
+			t.Log(board)
+			t.Errorf("board not contains %d", i)
+		}
+	}
+
+	if contains(&board, 9) {
+		t.Errorf("board should not contain %d", 9)
+	}
+}
+
 func TestInitBoard(t *testing.T) {
 	var board [9]int
 	initBoard(&board)
