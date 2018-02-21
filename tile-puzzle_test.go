bdb73185f0b01c86cfd83bfd57686d14782089a8
Roy Knäbel <roy.knaebel@gmx.de>
2018-02-21 20:39:05 +0100

    add move right unit test

--- tile-puzzle_test.go
+++ tile-puzzle_test.go
@@ -32,7 +32,6 @@ func TestInitBoard(t *testing.T) {
 }
 
 func TestLeft(t *testing.T) {
-
 	tables := []struct {
 		input  [9]int
 		output [9]int
@@ -50,6 +49,28 @@ func TestLeft(t *testing.T) {
 			t.Log(table.input)
 			t.Log(table.output)
 			t.Error("Move left failed.")
+		}
+	}
+}
+
+func TestRight(t *testing.T) {
+	tables := []struct {
+		input  [9]int
+		output [9]int
+		result bool
+	}{
+		{[9]int{1, 2, 3, 0, 4, 5, 7, 8, 6}, [9]int{1, 2, 3, 4, 0, 5, 7, 8, 6}, true},
+		{[9]int{1, 2, 3, 4, 0, 5, 7, 8, 6}, [9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, true},
+		{[9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, [9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, false},
+	}
+
+	for _, table := range tables {
+		result := right(&table.input)
+		if (table.input != table.output) && (result != table.result) {
+			t.Logf("right returned %t", result)
+			t.Log(table.input)
+			t.Log(table.output)
+			t.Error("Move right failed.")
 		}
 	}
 }
