2066eba5efe5691db6ddcb33940b9f0d61f84dc4
Roy Knäbel <roy.knaebel@gmx.de>
2018-03-06 21:22:00 +0100

    refactor unit tests

--- tile-puzzle_test.go
+++ tile-puzzle_test.go
@@ -31,90 +31,35 @@ func TestInitBoard(t *testing.T) {
 	}
 }
 
-func TestLeft(t *testing.T) {
+func TestMove(t *testing.T) {
 	tables := []struct {
 		input  [9]int
 		output [9]int
 		result bool
+		fn     func(*[9]int) bool
+		fnName string
 	}{
-		{[9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, [9]int{1, 2, 3, 4, 0, 5, 7, 8, 6}, true},
-		{[9]int{1, 2, 3, 4, 0, 5, 7, 8, 6}, [9]int{1, 2, 3, 0, 4, 5, 7, 8, 6}, true},
-		{[9]int{1, 2, 3, 0, 4, 5, 7, 8, 6}, [9]int{1, 2, 3, 0, 4, 5, 7, 8, 6}, false},
+		{[9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, [9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, true, left, "left"},
+		{[9]int{1, 2, 3, 4, 0, 5, 7, 8, 6}, [9]int{1, 2, 3, 0, 4, 5, 7, 8, 6}, true, left, "left"},
+		{[9]int{1, 2, 3, 0, 4, 5, 7, 8, 6}, [9]int{1, 2, 3, 0, 4, 5, 7, 8, 6}, false, left, "left"},
+		{[9]int{1, 2, 3, 0, 4, 5, 7, 8, 6}, [9]int{1, 2, 3, 4, 0, 5, 7, 8, 6}, true, right, "right"},
+		{[9]int{1, 2, 3, 4, 0, 5, 7, 8, 6}, [9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, true, right, "right"},
+		{[9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, [9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, false, right, "right"},
+		{[9]int{1, 2, 3, 4, 5, 6, 7, 0, 8}, [9]int{1, 2, 3, 4, 0, 6, 7, 5, 8}, true, up, "up"},
+		{[9]int{1, 2, 3, 4, 0, 6, 7, 5, 8}, [9]int{1, 0, 3, 4, 2, 6, 7, 5, 8}, true, up, "up"},
+		{[9]int{1, 0, 3, 4, 2, 6, 7, 5, 8}, [9]int{1, 0, 3, 4, 2, 6, 7, 5, 8}, false, up, "up"},
+		{[9]int{1, 0, 2, 3, 4, 5, 6, 7, 8}, [9]int{1, 4, 2, 3, 0, 5, 6, 7, 8}, true, down, "down"},
+		{[9]int{1, 4, 2, 3, 0, 5, 6, 7, 8}, [9]int{1, 4, 2, 3, 7, 5, 6, 0, 8}, true, down, "down"},
+		{[9]int{1, 4, 2, 3, 7, 5, 6, 0, 8}, [9]int{1, 4, 2, 3, 7, 5, 6, 0, 8}, false, down, "down"},
 	}
 
 	for _, table := range tables {
-		result := left(&table.input)
-		if (table.input != table.output) && (result != table.result) {
-			t.Logf("left returned %t", result)
+		result := table.fn(&table.input)
+		if result != table.result {
+			t.Logf("%s returned %t", table.fnName, result)
 			t.Log(table.input)
 			t.Log(table.output)
-			t.Error("Move left failed.")
-		}
-	}
-}
-
-func TestRight(t *testing.T) {
-	tables := []struct {
-		input  [9]int
-		output [9]int
-		result bool
-	}{
-		{[9]int{1, 2, 3, 0, 4, 5, 7, 8, 6}, [9]int{1, 2, 3, 4, 0, 5, 7, 8, 6}, true},
-		{[9]int{1, 2, 3, 4, 0, 5, 7, 8, 6}, [9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, true},
-		{[9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, [9]int{1, 2, 3, 4, 5, 0, 7, 8, 6}, false},
-	}
-
-	for _, table := range tables {
-		result := right(&table.input)
-		if (table.input != table.output) && (result != table.result) {
-			t.Logf("right returned %t", result)
-			t.Log(table.input)
-			t.Log(table.output)
-			t.Error("Move right failed.")
-		}
-	}
-}
-
-func TestUp(t *testing.T) {
-	tables := []struct {
-		input  [9]int
-		output [9]int
-		result bool
-	}{
-		{[9]int{1, 2, 3, 4, 5, 6, 7, 0, 8}, [9]int{1, 2, 3, 4, 0, 6, 7, 5, 8}, true},
-		{[9]int{1, 2, 3, 4, 0, 6, 7, 5, 8}, [9]int{1, 0, 3, 4, 2, 6, 7, 5, 8}, true},
-		{[9]int{1, 0, 3, 4, 2, 6, 7, 5, 8}, [9]int{1, 0, 3, 4, 2, 6, 7, 5, 8}, false},
-	}
-
-	for _, table := range tables {
-		result := up(&table.input)
-		if (table.input != table.output) && (result != table.result) {
-			t.Logf("up returned %t", result)
-			t.Log(table.input)
-			t.Log(table.output)
-			t.Error("Move up failed.")
-		}
-	}
-}
-
-func TestDown(t *testing.T) {
-	tables := []struct {
-		input  [9]int
-		output [9]int
-		result bool
-	}{
-		{[9]int{1, 0, 2, 3, 4, 5, 6, 7, 8}, [9]int{1, 4, 2, 3, 0, 5, 6, 7, 8}, true},
-		{[9]int{1, 4, 2, 3, 0, 5, 6, 7, 8}, [9]int{1, 4, 2, 3, 7, 5, 6, 0, 8}, true},
-		{[9]int{1, 4, 2, 3, 7, 5, 6, 0, 8}, [9]int{1, 4, 2, 3, 7, 5, 6, 0, 8}, false},
-	}
-
-	for _, table := range tables {
-		result := down(&table.input)
-		if (table.input != table.output) && (result != table.result) {
-			t.Logf("up returned %t", result)
-			t.Log(table.input)
-			t.Log(table.output)
-			t.Error("Move up failed.")
+			t.Errorf("Move %s failed", table.fnName)
 		}
 	}
 }
